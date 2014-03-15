69e62a17020954cec510eb47499c2e1b942032b0
Alfred Lee <crazy.jser@gmail.com>
2014-03-15 10:50:00 +0800

    resolve dependencives

--- libs/dawn_ui.js
+++ libs/dawn_ui.js
@@ -1,32 +1,42 @@
 #!/usr/bin/env node
-var express = require('express');
-var fs = require('fs');
-var path = require('path');
-var util = require('./util.js');
+var project = require('dawn-project');
 function Server () {}
 Server.prototype = {
   init: function initial ( env ) {
     'use strict';
-    console.log(env, 'initial');
     var root = this.root = env.parent.dir;
     var config = this.config = require(root + '/dawn-app.json');
-    var appConfig = config.app;
-    var staticConfig = config.statics;
-    var common = config.common;
-    var app = this.app = express();
-    if (!root || fs.existsSync(root)) {
-      util.error('The webroot [%s] is not a valid root', root);
+    var app = this.app = project(config.webroot);
+    // set view config (create an new server for active pages);
+    app.set('views', config.views);
+    // set static config (create an new server for combo or resource);
+    app.set('statics', config.statics);
+    // set view engine
+    app.set('view engine', 'ejs');
+    app.set('common', config.common);
+    // set the database
+    app.set('database', config.db);
+    app.use(project.favicon());
+    app.use(project.logger(env.parent.mode));
+    app.use(project.json());
+    app.use(project.urlencoded());
+    app.use(project.methodOverride());
+    app.use(project.router);
+    if (env.mode === 'dev') {
+      app.use(connect.errorHandler());
     }
-    if (!common || fs.existsSync(root + '/' + common)) {
-      util.error('common module [%s] is not exists.', common);
-    }
-    
+    app.run();
   },
   start: function () {
-
+    if (this.app.RUNNING) {
+      return;
+    }
+    this.app.run();
   },
   stop: function () {
-
+    if (this.app.RUNNING) {
+      this.app.stop();
+    }
   }
 };
 exports = module.exports = new Server();
